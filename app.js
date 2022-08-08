450d15c7e76251e9a841dbb3b36a721a82e2baff
Vinícius Gonçalves <oldz.registros@gmail.com>
2022-08-08 19:22:26 -0300

    Created a method for insert data

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const request = indexedDB.open('cryptocoins', 1)
 
+const insertData = (db, newObject) => {
+    const transaction = db.transaction('cryptos', 'readwrite')
+    const store = transaction.objectStore('cryptos')
+    const query = store.put({ ...newObject })
+    transaction.addEventListener('complete', () => db.close())
+}
+
 request.addEventListener('success', event => {
     const db = event.target.result
-    console.log('Success on indexedDB open request', db)
+    insertData(db, { name: 'Bitcoin', release_date: '2009-january'})
 })
 
 request.addEventListener('error', event => {
@@ -15,7 +22,7 @@ request.addEventListener('upgradeneeded', event => {
         const db = event.target.result
         const { objectStoreNames } = db
 
-        if(objectStoreNames.contains('cryptos')) {
+        if(!objectStoreNames.contains('cryptos')) {
             db.createObjectStore('cryptos', { autoIncrement: true })
         }
 
