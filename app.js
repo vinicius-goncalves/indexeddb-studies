b9b9cecd67b32a30ae4ec218621a972b7e1bfd4b
Vinícius Gonçalves <oldz.registros@gmail.com>
2022-08-08 19:35:56 -0300

    Some tests with cursor

--- app.js
+++ app.js
@@ -3,13 +3,36 @@ const request = indexedDB.open('cryptocoins', 1)
 const insertData = (db, newObject) => {
     const transaction = db.transaction('cryptos', 'readwrite')
     const store = transaction.objectStore('cryptos')
-    const query = store.put({ ...newObject })
+    store.put({ ...newObject })
+    
     transaction.addEventListener('complete', () => db.close())
 }
 
 request.addEventListener('success', event => {
-    const db = event.target.result
-    insertData(db, { name: 'Bitcoin', release_date: '2009-january'})
+
+    const createNewItem = new Promise(resolve => {
+        const db = event.target.result
+        insertData(db, { name: 'Bitcoin', release_date: '2009-january'})
+        resolve(db)
+    })
+
+    createNewItem.then(db => {
+        const transaction = db.transaction('cryptos', 'readonly')
+        const store = transaction.objectStore('cryptos')
+        
+        store.openCursor().addEventListener('success', event => {
+            const { ['result']: cursor } = event.target
+            if(!cursor) {
+                return
+            }
+
+            const crypto = cursor.value
+            console.log(crypto)
+            cursor.continue()
+        })
+
+        transaction.addEventListener('complete', () => db.close())
+    })
 })
 
 request.addEventListener('error', event => {
@@ -24,6 +47,7 @@ request.addEventListener('upgradeneeded', event => {
 
         if(!objectStoreNames.contains('cryptos')) {
             db.createObjectStore('cryptos', { autoIncrement: true })
+            return
         }
 
     } catch (error) {
