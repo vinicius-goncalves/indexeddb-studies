195c5745dc68521ab361bd5d0de03be86ddd0562
Vinícius Gonçalves <oldz.registros@gmail.com>
2022-08-09 12:20:33 -0300

    Code has rewritten

--- app.js
+++ app.js
@@ -1,56 +1,50 @@
-const request = indexedDB.open('cryptocoins', 1)
-
-const insertData = (db, newObject) => {
-    const transaction = db.transaction('cryptos', 'readwrite')
-    const store = transaction.objectStore('cryptos')
-    store.put({ ...newObject })
+(() => {
     
-    transaction.addEventListener('complete', () => db.close())
-}
+    const request = indexedDB.open('cryptocoins', 1)
+    
+    const dbPromise = new Promise((resolve, reject) => {
 
-request.addEventListener('success', event => {
+        request.addEventListener('success', (event) => {
+            const db = event.target.result
+            resolve(db)
+        })
 
-    const createNewItem = new Promise(resolve => {
-        const db = event.target.result
-        insertData(db, { name: 'Bitcoin', release_date: '2009-january'})
-        resolve(db)
+        request.addEventListener('upgradeneeded', (event) => {
+            const db = event.target.result
+            const { objectStoreNames } = db
+            if(!objectStoreNames.contains('coins')) {
+                return db.createObjectStore('coins', { autoIncrement: true })
+            }
+        })
+
+        request.addEventListener('error', (event) => reject(event))
+
     })
 
-    createNewItem.then(db => {
-        const transaction = db.transaction('cryptos', 'readonly')
-        const store = transaction.objectStore('cryptos')
-        
+    dbPromise.then(db => {
+        const transaction = db.transaction('coins', 'readwrite')
+        const store = transaction.objectStore('coins')
+        const query = store.get(2)
+
+        query.addEventListener('success', event => {
+            console.log(event.target)
+        })
+
+        store.put({ name: 'Bitcoin' })
+
         store.openCursor().addEventListener('success', event => {
             const { ['result']: cursor } = event.target
+
             if(!cursor) {
                 return
             }
-
-            const crypto = cursor.value
-            console.log(crypto)
+            
+            const value = cursor.value
+            console.log(value)
             cursor.continue()
+
         })
 
         transaction.addEventListener('complete', () => db.close())
     })
-})
-
-request.addEventListener('error', event => {
-    console.log('Error on indexedDB close request', event.target.result)
-})
-
-request.addEventListener('upgradeneeded', event => {
-    try {
-
-        const db = event.target.result
-        const { objectStoreNames } = db
-
-        if(!objectStoreNames.contains('cryptos')) {
-            db.createObjectStore('cryptos', { autoIncrement: true })
-            return
-        }
-
-    } catch (error) {
-        console.log(error)
-    }
-})
+})();
