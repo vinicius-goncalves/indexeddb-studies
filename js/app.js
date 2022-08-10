62d346e20747f8c0b23b475ea06f83ada098858d
Vinícius Gonçalves <oldz.registros@gmail.com>
2022-08-10 07:56:28 -0300

    Added internal methods such as 'log' and 'random-array-item'. Added external buttons such as get all object stores and add random object to store

---
+++ js/app.js
@@ -0,0 +1,79 @@
+const request = indexedDB.open('cryptocoins', 1)
+
+import { cryptos } from './cryptos.js'
+
+const methods = method => {
+    
+    if(method === 'log') {
+        return new Promise(resolve => {
+            const log = (...info) => console.log(...info)
+            resolve(log)
+        })
+    }
+
+    if(method === 'randomArrayItem') {
+        return new Promise(resolve => {
+            const randomItem = (array) => array[Math.floor(Math.random() * array.length)]
+            resolve(randomItem)
+        })
+
+    }
+} 
+
+const getAllStores = document.querySelector('[data-button="get-all-stores"]')
+const addRandomValue = document.querySelector('[data-button="add-random-value"]')
+
+
+const dbPromise = new Promise((resolve, reject) => {
+
+    request.addEventListener('success', (event) => {
+        const db = event.target.result
+        resolve(db)
+    })
+
+    request.addEventListener('upgradeneeded', (event) => {
+        const db = event.target.result
+        const { objectStoreNames } = db
+        if(!objectStoreNames.contains('coins')) {
+            return db.createObjectStore('coins', { autoIncrement: true })
+        }
+    })
+
+    request.addEventListener('error', (event) => reject(event))
+})
+
+getAllStores.addEventListener('click', () => {
+    dbPromise.then(db => {
+        const transaction = db.transaction('coins', 'readwrite')
+        const store = transaction.objectStore('coins')
+        const openCursor = store.openCursor()
+
+        openCursor.addEventListener('success', (event) => {
+
+            const { ['result']: cursor } = event.target
+            if(!cursor) { return }
+
+            const coin = cursor.value
+            log(coin)
+            cursor.continue()
+
+        })
+    })
+})
+
+addRandomValue.addEventListener('click', async () => {
+
+    const randomItem = await methods('randomArrayItem').then(r_a_i => r_a_i(cryptos))
+    const log = await methods('log')
+
+    const db = await dbPromise
+    const transaction = db.transaction('coins', 'readwrite')
+    const store = transaction.objectStore('coins')
+    const query = store.put({...randomItem})
+    
+    query.addEventListener('success', () => log('A new item has been added succefully'))
+    query.addEventListener('error', (event) => log('An error occurred.', event))
+
+    transaction.addEventListener('complete', (event) => log('The transaction has been completed succefully', event))
+    
+})
---
+++ js/cryptos.js
@@ -0,0 +1,11 @@
+export const cryptos = [
+    { name: 'Bitcoin', symbol: 'BTC', circulating_supply: '~19M', type: 'cryptocurrency' },
+    { name: 'Ethereum', symbol: 'ETH', circulating_supply: '~121M', type: 'token' },
+    { name: 'BNB', symbol: 'BNB', circulating_supply: '~161M', type: 'cryptocurrency' },
+    { name: 'XRP', symbol: 'XRP', circulating_supply: '~48B', type: 'token' },
+    { name: 'TRON', symbol: 'TRX', circulating_supply: '~92B', type: 'token' },
+    { name: 'Monero', symbol: 'XMR', circulating_supply: '~18M', type: 'cryptocurrency' },
+    { name: 'Chiliz', symbol: 'CHZ', circulating_supply: '~6B', type: 'token' },
+    { name: 'BitTorrent', symbol: 'BTT', circulating_supply: '~19M', type: 'token' },
+    { name: 'Litecoin', symbol: 'LTC', circulating_supply: '~70M', type: 'cryptocurrency' }
+]
